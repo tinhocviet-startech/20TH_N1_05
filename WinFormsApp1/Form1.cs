53ff0edb451c3e21855bfe881ab565eacf571d06
“trung” <startech.edu.vn@gmail.com>
2024-03-16 15:40:20 +0700

    thêm nội dung

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,5 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -6,5 +8,11 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+        "Từ ấy trong tôi bừng nắng hạ
+Mặt trời chân lý chói qua tim
+Hồn tôi là một vườn hoa lá
+Rất đậm hương và rộn tiếng chim...
+
+
     }
 }
